00ef57cffe95cd81b13372efbe3a6d8167ddf5d5
Ross <145527274+ross-ap@users.noreply.github.com>
2025-03-11 11:51:55 -0400

    Finished assessment part 2

--- AsyncProgrammingAssessments/01-asynchronous-programming/02/02-n-files-synchronously.js
+++ AsyncProgrammingAssessments/01-asynchronous-programming/02/02-n-files-synchronously.js
@@ -2,10 +2,30 @@
 =======================
 02-n-files-synchronously.js
 =======================
-Student ID:
+Student ID: 23783185
 Comment (Required):
 
 =======================
 */
 const fs = require("fs");
 const n = 5;	//input size 0 < n < 100
+const data = "Data-2";
+let written = 0;
+const outputDirectory = "./output"
+
+writeSync(1);
+function writeSync(i){
+    let fileName = `${i.toString().padStart(2, "0")}-output.txt`;
+    let path = `${outputDirectory}/${fileName}`;
+    fs.writeFile(path, data, (err) => after_write(i, fileName));
+    
+}
+
+function after_write(i, fileName){
+    console.log(`Finished writing ${fileName}`)
+    if(i >= n){ 
+        console.log("Writing complete");
+    } else {
+        writeSync(i+1);
+    }
+}
